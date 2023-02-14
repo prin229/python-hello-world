2843cccce30ccd90d3c530c1aefd6ba7bbf371d7
prin229 <38900749+prin229@users.noreply.github.com>
2023-02-14 14:26:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import Flask , render_template
 app = Flask(__name__)
 
 def printres():
-	cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=sql-demo-serv.database.windows.net;DATABASE=sql-demo;UID=prince;PWD=Azure@feb2023')
-	cursor = cnxn.cursor()	
-	cursor.execute("select * from demo_tb") 
-	row = cursor.fetchone() 
-	while row:
-		print (row) 
-		row = cursor.fetchone()
-		return row
+	server = 'sql-demo-serv.database.windows.net'
+	database = 'sql-demo'
+	username = 'prince'
+	password = 'Azure@feb2023'   
+	driver= '{ODBC Driver 18 for SQL Server}'
+
+	with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+		with conn.cursor() as cursor:
+			cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+			row = cursor.fetchone()
+			while row:
+				print (str(row[0]) + " " + str(row[1]))
+				row = cursor.fetchone()
 
 
 @app.route("/")
