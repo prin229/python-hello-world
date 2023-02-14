700749b1a5818e5aa7ea12a7f319461071a86ac0
prin229 <38900749+prin229@users.noreply.github.com>
2023-02-14 22:14:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ def printres():
 	username = 'prince'
 	password = 'Azure@feb2023'   
 	driver= '{ODBC Driver 18 for SQL Server}'
-
-	with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-		with conn.cursor() as cursor:
-			cursor.execute("select id from demo_tb")
-			row = cursor.fetchone()
-			while row:
-				print (str(row[0]) + " " )
+	try:
+		with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+			with conn.cursor() as cursor:
+				cursor.execute("SELECT id from demo_tb")
 				row = cursor.fetchone()
+				while row:
+					print (str(row[0]) + " " + str(row[1]))
+					row = cursor.fetchone()
+	except Exception as e:
+		      print('error is : '+str(e))
 
 
 @app.route("/")
