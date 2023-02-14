69db7b01af320a282ed71b1e46862061d5db0ebf
prin229 <38900749+prin229@users.noreply.github.com>
2023-02-14 13:55:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import pyodbc 
 from flask import Flask , render_template
 app = Flask(__name__)
 
+def printres():
+	cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=sql-demo-serv.database.windows.net;DATABASE=sql-demo;UID=prince;PWD=Azure@feb2023')
+	cursor = cnxn.cursor()	
+	cursor.execute("select * from demo_tb") 
+	row = cursor.fetchone() 
+	while row:
+		print (row) 
+		row = cursor.fetchone()
+		return row
+
+
 @app.route("/")
 def hello():
-    return render_template('profile-tool.html')
+    return printres()
