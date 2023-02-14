bce5398f5fbcafb0b04b75f8f9ad23131de6b5e0
prin229 <38900749+prin229@users.noreply.github.com>
2023-02-14 22:22:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def printres():
 	username = 'prince'
 	password = 'Azure@feb2023'   
 	driver= '{ODBC Driver 18 for SQL Server}'
-	try:
+	return driver
+	"""try:
 		with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
 			with conn.cursor() as cursor:
 				cursor.execute("SELECT id from demo_tb")
@@ -17,7 +18,7 @@ def printres():
 					print (str(row[0]) + " " + str(row[1]))
 					row = cursor.fetchone()
 	except Exception as e:
-		      print('error is : '+str(e))
+		      print('error is : '+str(e))"""
 
 
 @app.route("/")
