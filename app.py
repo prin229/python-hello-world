fedc438ef300e291f44f68aa6d02158e6d31a6a6
prin229 <38900749+prin229@users.noreply.github.com>
2023-02-21 14:39:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,77 @@
 import pyodbc 
+import os
+import uuid
+import sys
+import csv
+from azure.storage.blob import BlockBlobService, PublicAccess
 from flask import Flask , render_template,request
 app = Flask(__name__)
+
+
+def run_sample():
+    try:
+        # Create the BlockBlobService that is used to call the Blob service for the storage account
+        blob_service_client = BlockBlobService(
+            account_name='princeaccountstorage', account_key='rMESDBSw8Xq0xDvtIbZZBSHW3XOL9bkVrRxqJ10+sXLPEkROfEd1ldsI9Y+yXSgJyh2fry7Wtsq2+AStcb4O+Q==')
+
+        # Create a container called 'quickstartblobs'.
+        container_name = 'quickstartblobs'
+        blob_service_client.create_container(container_name)
+
+        # Set the permission so the blobs are public.
+        blob_service_client.set_container_acl(
+            container_name, public_access=PublicAccess.Container)
+
+        # Create Sample folder if it not exists, and create a file in folder Sample to test the upload and download.
+        local_path = os.path.expanduser("~/Sample")
+        if not os.path.exists(local_path):
+            os.makedirs(os.path.expanduser("~/Sample"))
+        local_file_name = "QuickStart_" + str(uuid.uuid4()) + ".csv"
+        full_path_to_file = os.path.join(local_path, local_file_name)
+
+		# Write text to the file.
+        header = ['name', 'area', 'country_code2', 'country_code3']
+        data = ['Afghanistan', 652090, 'AF', 'AFG']
+        file = open(full_path_to_file,  'w',encoding='UTF8')
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerow(data)
+        file.close()
+
+        print("Temp file = " + full_path_to_file)
+        print("\nUploading to Blob storage as blob" + local_file_name)
+
+        # Upload the created file, use local_file_name for the blob name
+        blob_service_client.create_blob_from_path(
+            container_name, 'input/'+local_file_name, full_path_to_file)
+
+        # List the blobs in the container
+        #print("\nList blobs in the container")
+        #generator = blob_service_client.list_blobs(container_name)
+        #for blob in generator:
+            #print("\t Blob name: " + blob.name)
+
+        # Download the blob(s).
+        # Add '_DOWNLOADED' as prefix to '.txt' so you can see both files in Documents.
+        #full_path_to_file2 = os.path.join(local_path, str.replace(
+        #   local_file_name ,'.txt', '_DOWNLOADED.txt'))
+        #print("\nDownloading blob to " + full_path_to_file2)
+        #blob_service_client.get_blob_to_path(
+            #container_name, local_file_name, full_path_to_file2)
+
+        #sys.stdout.write("Sample finished running. When you hit <any key>, the sample will be deleted and the sample "
+        #                 "application will exit.")
+        #sys.stdout.flush()
+        #input()
+
+        # Clean up resources. This includes the container and the temp files
+        #blob_service_client.delete_container(container_name)
+        os.remove(full_path_to_file)
+        return 'successfully uploaded'
+        #os.remove(full_path_to_file2)
+    except Exception as e:
+        print(e)
+        return 'failed..'+str(e)
 
 def printres():
 	server = 'sql-demo-serv.database.windows.net'
@@ -22,6 +93,7 @@ def printres():
 	except Exception as e:
 		      print('error is : '+str(e))
 
+#run_sample()
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
 	table_name = request.form['table']
@@ -47,6 +119,6 @@ def handle_data():
 
 @app.route("/")
 def hello():
-    return render_template('profile-tool.html')
+    return run_sample()
 	
 
